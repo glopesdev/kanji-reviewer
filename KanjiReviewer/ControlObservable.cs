c34590c38817203366c1b9559023ec53200faa58
glopesdev <glopesdev@users.noreply.github.com>
2017-04-02 19:51:34 +0100

    Added basic Rx event sources.

---
+++ KanjiReviewer/ControlObservable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace KanjiReviewer
+{
+    static class ControlObservable
+    {
+        public static IObservable<EventArgs> FromClick(AppBarButton button)
+        {
+            return Observable.FromEvent<RoutedEventHandler, EventArgs>(
+                handler => button.Click += handler,
+                handler => button.Click -= handler);
+        }
+
+        public static IObservable<TResult> FromClick<TResult>(AppBarButton button, TResult result)
+        {
+            return FromClick(button).Select(evt => result);
+        }
+    }
+}
