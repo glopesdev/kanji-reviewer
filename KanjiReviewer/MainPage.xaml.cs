23cc72840ec6399595758c9c5f0fec4a0c155526
glopesdev <glopesdev@users.noreply.github.com>
2017-04-02 22:05:40 +0100

    Added inking canvas, kanji viewbox and report buttons.

--- KanjiReviewer/MainPage.xaml.cs
+++ KanjiReviewer/MainPage.xaml.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Input.Inking;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
@@ -25,6 +31,16 @@ namespace KanjiReviewer
         public MainPage()
         {
             this.InitializeComponent();
+
+            var drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Colors.Black;
+            drawingAttributes.IgnorePressure = true;
+            drawingAttributes.FitToCurve = false;
+            kanjiCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            kanjiCanvas.InkPresenter.InputDeviceTypes =
+                CoreInputDeviceTypes.Mouse |
+                CoreInputDeviceTypes.Pen |
+                CoreInputDeviceTypes.Touch;
         }
     }
 }
